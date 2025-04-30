af88eccdb813be41ba969af416655da180a6fb81
jimwang <jimwang@ucsb.edu>
2025-04-29 17:50:52 -0700

    fixtures

---
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -0,0 +1,36 @@
+const organizationFixtures = {
+    oneOrganization: [
+      {
+        orgCode: "foo",
+        orgTranslationShort: "Foo",
+        orgTranslation: "FooBar",
+        inactive: false
+      },
+    ],
+  
+    threeOrganizations: [
+      {
+        orgCode: "foo",
+        orgTranslationShort: "Foo",
+        orgTranslation: "FooBar",
+        inactive: false
+      },
+  
+      {
+        orgCode: "too",
+        orgTranslationShort: "Too",
+        orgTranslation: "TooFar",
+        inactive: false
+      },
+  
+      {
+        orgCode: "goo",
+        orgTranslationShort: "Goo",
+        orgTranslation: "GooTar",
+        inactive: false
+      },
+    ],
+  };
+  
+  export { organizationFixtures };
+  
