6d43f605f39d9947449df6212806e233b7cb45de
hienhuynh2026 <hienhuynh@ucsb.edu>
2025-04-30 02:02:10 -0700

    fixed formatting issue and also fixed a return error for recommendation request fixtures

--- frontend/src/fixtures/recommendationRequestsFixtures.js
+++ frontend/src/fixtures/recommendationRequestsFixtures.js
@@ -1,42 +1,42 @@
 const RecommendationRequestFixtures = {
   oneRecommendationRequest: {
-    "id": 2,
-    "requestorEmail": "kevinlee@ucsb.edu",
-    "professorEmail": "zmatni@ucsb.edu",
-    "explanation": "Recommendation 1 ",
-    "dateRequested": "2025-04-30T05:50:50",
-    "dateNeeded": "2025-04-30T06:50:50",
-    "done": false
+    id: 2,
+    requestorEmail: "kevinlee@ucsb.edu",
+    professorEmail: "zmatni@ucsb.edu",
+    explanation: "Recommendation 1 ",
+    dateRequested: "2025-04-30T05:50:50",
+    dateNeeded: "2025-04-30T06:50:50",
+    done: false,
   },
-  threeRecommendationRequest:[
+  threeRecommendationRequest: [
     {
-      "id": 2,
-      "requestorEmail": "kevinlee@ucsb.edu",
-      "professorEmail": "zmatni@ucsb.edu",
-      "explanation": "Recommendation 1 ",
-      "dateRequested": "2025-04-30T05:50:50",
-      "dateNeeded": "2025-04-30T06:50:50",
-      "done": false
+      id: 2,
+      requestorEmail: "kevinlee@ucsb.edu",
+      professorEmail: "zmatni@ucsb.edu",
+      explanation: "Recommendation 1 ",
+      dateRequested: "2025-04-30T05:50:50",
+      dateNeeded: "2025-04-30T06:50:50",
+      done: false,
     },
     {
-      "id": 3,
-      "requestorEmail": "kevinlee@ucsb.edu",
-      "professorEmail": "zmatni@ucsb.edu",
-      "explanation": "Recommendation 2",
-      "dateRequested": "2025-05-30T05:50:50",
-      "dateNeeded": "2025-05-31T06:50:50",
-      "done": false
+      id: 3,
+      requestorEmail: "kevinlee@ucsb.edu",
+      professorEmail: "zmatni@ucsb.edu",
+      explanation: "Recommendation 2",
+      dateRequested: "2025-05-30T05:50:50",
+      dateNeeded: "2025-05-31T06:50:50",
+      done: false,
     },
     {
-      "id": 4,
-      "requestorEmail": "sauldiaz@ucsb.edu",
-      "professorEmail": "fgibou@ucsb.edu",
-      "explanation": "Recommendation 3",
-      "dateRequested": "2026-05-30T05:50:50",
-      "dateNeeded": "2027-05-31T06:50:50",
-      "done": true
-    }
-  ]
+      id: 4,
+      requestorEmail: "sauldiaz@ucsb.edu",
+      professorEmail: "fgibou@ucsb.edu",
+      explanation: "Recommendation 3",
+      dateRequested: "2026-05-30T05:50:50",
+      dateNeeded: "2027-05-31T06:50:50",
+      done: true,
+    },
+  ],
 };
 
-export { ucsbDatesFixtures };
+export { RecommendationRequestFixtures };
