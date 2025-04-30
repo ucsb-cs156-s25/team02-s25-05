80562cb48da6aeb7e67f47fe5ff78fa01c287ca5
Austin Quach <austinquach@ucsb.edu>
2025-04-29 17:45:04 -0700

    Created fixtures

---
+++ frontend/src/fixtures/UCSBDiningCommonsMenuItemFixture.js
@@ -0,0 +1,30 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+  oneItem: {
+    id: 1,
+    diningCommonsCode: "ortega",
+    name: "chicken burrito",
+    station: "burrito bar",
+  },
+  threeItems: [
+    {
+      id: 1,
+      diningCommonsCode: "portola",
+      name: "chocolate milk",
+      station: "beverages",
+    },
+    {
+      id: 2,
+      diningCommonsCode: "carillo",
+      name: "salad",
+      station: "salad bar",
+    },
+    {
+      id: 3,
+      diningCommonsCode: "ortega",
+      name: "vanilla ice cream",
+      station: "ice cream machine",
+    },
+  ],
+};
+
+export { ucsbDiningCommonsMenuItemFixtures };
