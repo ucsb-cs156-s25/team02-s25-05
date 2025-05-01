74ea68f7653f7b443a98604b475431d5a0f6a096
hienhuynh2026 <hienhuynh@ucsb.edu>
2025-05-01 03:57:05 -0700

    hienhuynh2026 - add RecommendationRequestTable, tests and stories

---
+++ frontend/src/stories/components/RecommendationRequest/RecommendationRequestTable.stories.js
@@ -0,0 +1,45 @@
+import React from "react";
+import RecommendationRequestTable from "main/components/RecommendationRequests/RecommendationRequestTable";
+import { RecommendationRequestFixtures } from "fixtures/recommendationRequestsFixtures";
+import { currentUserFixtures } from "fixtures/currentUserFixtures";
+import { http, HttpResponse } from "msw";
+
+export default {
+  title: "components/RecommendationRequests/RecommendationRequestTable",
+  component: RecommendationRequestTable,
+};
+
+const Template = (args) => {
+  return <RecommendationRequestTable {...args} />;
+};
+
+export const Empty = Template.bind({});
+
+Empty.args = {
+  recommendationrequests: [],
+  currentUser: currentUserFixtures.userOnly,
+};
+
+export const ThreeItemsOrdinaryUser = Template.bind({});
+
+ThreeItemsOrdinaryUser.args = {
+  recommendationrequests: RecommendationRequestFixtures.threeRecommendationRequest,
+  currentUser: currentUserFixtures.userOnly,
+};
+
+export const ThreeItemsAdminUser = Template.bind({});
+ThreeItemsAdminUser.args = {
+  recommendationrequests: RecommendationRequestFixtures.threeRecommendationRequest,
+  currentUser: currentUserFixtures.adminUser,
+};
+
+ThreeItemsAdminUser.parameters = {
+  msw: [
+    http.delete("/api/recommendationrequest", () => {
+      return HttpResponse.json(
+        { message: "Recommendation request deleted successfully" },
+        { status: 200 },
+      );
+    }),
+  ],
+};
