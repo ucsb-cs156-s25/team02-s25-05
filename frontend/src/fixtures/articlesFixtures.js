f1a62502309cf1ab2efe3385d6cdef301806b1a9
Vishal S <vseenivasan@ucsb.edu>
2025-04-30 18:37:08 -0700

    Fixed code style issues

--- frontend/src/fixtures/articlesFixtures.js
+++ frontend/src/fixtures/articlesFixtures.js
@@ -1,39 +1,38 @@
 const articlesFixtures = {
-    oneArticle: {
-        "id": 1,
-        "title": "My first article",
-        "url": "firstArticle.com",
-        "explanation": "The first test article",
-        "email": "first@email.com",
-        "dateAdded": "2025-04-30T00:00:00"
-      },
-    threeArticles: [
+  oneArticle: {
+    id: 1,
+    title: "My first article",
+    url: "firstArticle.com",
+    explanation: "The first test article",
+    email: "first@email.com",
+    dateAdded: "2025-04-30T00:00:00",
+  },
+  threeArticles: [
     {
-        "id": 1,
-        "title": "Primary",
-        "url": "primary.com",
-        "explanation": "The first of the three articles",
-        "email": "primary@email.com",
-        "dateAdded": "2025-01-01T00:00:00"
+      id: 1,
+      title: "Primary",
+      url: "primary.com",
+      explanation: "The first of the three articles",
+      email: "primary@email.com",
+      dateAdded: "2025-01-01T00:00:00",
     },
     {
-        "id": 2,
-        "title": "Secondary",
-        "url": "secondary.com",
-        "explanation": "The second of the three articles",
-        "email": "secondary@email.com",
-        "dateAdded": "2025-02-02T00:00:00"
-      },
-      {
-        "id": 3,
-        "title": "Tertiary",
-        "url": "tertiary.com",
-        "explanation": "The third of the three articles",
-        "email": "tertiary@email.com",
-        "dateAdded": "2025-03-03T00:00:00"
-      },
-    ],
-  };
-  
-  export { articlesFixtures };
-  
+      id: 2,
+      title: "Secondary",
+      url: "secondary.com",
+      explanation: "The second of the three articles",
+      email: "secondary@email.com",
+      dateAdded: "2025-02-02T00:00:00",
+    },
+    {
+      id: 3,
+      title: "Tertiary",
+      url: "tertiary.com",
+      explanation: "The third of the three articles",
+      email: "tertiary@email.com",
+      dateAdded: "2025-03-03T00:00:00",
+    },
+  ],
+};
+
+export { articlesFixtures };
