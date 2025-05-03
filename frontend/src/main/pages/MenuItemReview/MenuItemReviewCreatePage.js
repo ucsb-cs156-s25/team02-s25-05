99f33412b01ad6fddfd1af7fcc4d8d5f0bd51b75
Michael Tsai <miketsai00@gmail.com>
2025-05-03 07:48:43 -0700

    MT - adjusted create page print message

--- frontend/src/main/pages/MenuItemReview/MenuItemReviewCreatePage.js
+++ frontend/src/main/pages/MenuItemReview/MenuItemReviewCreatePage.js
@@ -18,7 +18,7 @@ export default function MenuItemReviewCreatePage({ storybook = false }) {
   });
 
   const onSuccess = (menuItemReview) => {
-    toast(`New menuItemReview Created - id: ${menuItemReview.id} name: ${menuItemReview.name}`);
+    toast(`New menuItemReview Created - id: ${menuItemReview.id} comments: ${menuItemReview.comments}`);
   };
 
   const mutation = useBackendMutation(
