a04c2a701a99982322f0c2ee942be425717ab636
Vishal S <vseenivasan@ucsb.edu>
2025-05-05 06:47:17 -0700

    Added tests for ArticlesUtils

--- frontend/src/stories/components/Articles/ArticlesTable.stories.js
+++ frontend/src/stories/components/Articles/ArticlesTable.stories.js
@@ -35,7 +35,7 @@ ThreeItemsAdminUser.args = {
 ThreeItemsAdminUser.parameters = {
   msw: [
     http.delete("/api/articles", () => {
-      return HttpResponse.json({}, { status: 200 });
+      return HttpResponse.json({message: "Date deleted successfully"}, { status: 200 });
     }),
   ],
 };
