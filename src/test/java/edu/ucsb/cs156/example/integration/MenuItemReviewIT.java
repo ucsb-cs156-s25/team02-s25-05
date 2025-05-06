f16662415a00e5475ac8f3ea4cfe744733725a0c
Michael Tsai <miketsai00@gmail.com>
2025-05-06 11:43:46 -0700

    MT - typo fix

--- src/test/java/edu/ucsb/cs156/example/integration/MenuItemReviewIT.java
+++ src/test/java/edu/ucsb/cs156/example/integration/MenuItemReviewIT.java
@@ -30,7 +30,7 @@ import edu.ucsb.cs156.example.services.CurrentUserService;
 import edu.ucsb.cs156.example.services.GrantedAuthoritiesService;
 import edu.ucsb.cs156.example.testconfig.TestConfig;
 
-import java.time.localDateTime;
+import java.time.LocalDateTime;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
