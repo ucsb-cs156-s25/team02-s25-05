4d0c09fd310ecaad785490724279238e85977869
Austin Quach <austinquach@ucsb.edu>
2025-05-05 21:35:48 -0700

    end to end tests passing

---
+++ src/test/java/edu/ucsb/cs156/example/web/UCSBDiningCommonsMenuItemWebIT.java
@@ -0,0 +1,57 @@
+package edu.ucsb.cs156.example.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+import edu.ucsb.cs156.example.WebTestCase;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("integration")
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
+public class UCSBDiningCommonsMenuItemWebIT extends WebTestCase {
+    @Test
+    public void admin_user_can_create_edit_delete_menu_item() throws Exception {
+        setupUser(true);
+
+        page.getByText("UCSBDiningCommonsMenuItem").click();
+
+        page.getByText("Create Menu Item").click();
+        assertThat(page.getByText("Create New Menu Item")).isVisible();
+        page.getByTestId("UCSBDiningCommonsMenuItemForm-diningCommonsCode").selectOption("Carrillo");
+        page.getByTestId("UCSBDiningCommonsMenuItemForm-name").fill("Pineapple");
+        page.getByTestId("UCSBDiningCommonsMenuItemForm-station").fill("Fruit Bar");
+        page.getByTestId("UCSBDiningCommonsMenuItemForm-submit").click();
+
+        assertThat(page.getByTestId("UCSBDiningCommonsMenuItemTable-cell-row-0-col-name"))
+                .hasText("Pineapple");
+
+        page.getByTestId("UCSBDiningCommonsMenuItemTable-cell-row-0-col-Edit-button").click();
+        assertThat(page.getByText("Edit Menu Item")).isVisible();
+        page.getByTestId("UCSBDiningCommonsMenuItemForm-station").fill("NEW STATION");
+        page.getByTestId("UCSBDiningCommonsMenuItemForm-submit").click();
+
+        assertThat(page.getByTestId("UCSBDiningCommonsMenuItemTable-cell-row-0-col-station")).hasText("NEW STATION");
+
+        page.getByTestId("UCSBDiningCommonsMenuItemTable-cell-row-0-col-Delete-button").click();
+
+        assertThat(page.getByTestId("UCSBDiningCommonsMenuItemTable-cell-row-0-col-name")).not().isVisible();
+    }
+
+    @Test
+    public void regular_user_cannot_create_restaurant() throws Exception {
+        setupUser(false);
+
+        page.getByText("UCSBDiningCommonsMenuItem").click();
+
+        assertThat(page.getByText("Create Menu Item")).not().isVisible();
+        assertThat(page.getByTestId("UCSBDiningCommonsMenuItemTable-cell-row-0-col-name")).not().isVisible();
+    }
+}
